4339438d5c0e222652c0479d77db88283a71894c
SV9050 <1090067@pdsb.net>
2025-03-07 14:29:19 -0500

    initial commit

--- 2_Challenges/2_guess_the_number/guess_the_number.py
+++ 2_Challenges/2_guess_the_number/guess_the_number.py
@@ -11,7 +11,7 @@ import random
 # Generate a random number between 1 and 10
 Random_Number = random.randint(1, 10)
 
-# Start the guessing loop
+# use the loop
 while True :
     # Ask the user to guess the number
     guess = int(input("Guess a number between 1 and 10: "))
