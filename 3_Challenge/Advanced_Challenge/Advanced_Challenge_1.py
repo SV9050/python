e01c4ca29108acf67159c6dd1f6267010357a01b
SV9050 <1090067@pdsb.net>
2025-03-19 14:24:22 -0400

    initial commit

---
+++ 3_Challenge/Advanced_Challenge/Advanced_Challenge_1.py
@@ -0,0 +1,13 @@
+#-----------------------------------------------------------------------------
+# Name:        Day-of-the-Week-Activity-Recommender
+# Purpose:     recommending activities
+# Author:      Sachin Vijay
+# Created:     25-Feb-2025
+# Updated:     25-Feb-2025
+#---------------------------------------------------
+
+fruits = ['banana', 'apple', 'grape', 'kiwi', 'orange']
+fruits.sort()
+print(fruits, 'in ascending order')
+fruits.reverse()
+print(fruits, 'in descending order')
