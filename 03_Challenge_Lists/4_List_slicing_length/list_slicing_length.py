48b46c97d070bd9560f9d7d20fd221c76fdac1f9
SV9050 <1090067@pdsb.net>
2025-03-24 13:03:41 -0400

    initial commit

--- 03_Challenge_Lists/4_List_slicing_length/list_slicing_length.py
+++ 03_Challenge_Lists/4_List_slicing_length/list_slicing_length.py
@@ -6,9 +6,21 @@
 # Updated:     19-Mar-2025
 # ---------------------------------------------------
 
+# Creating colors list
 colors = ['red', 'blue', 'green', 'yellow', 'purple']
+print (colors)
+
+# slicing/extracting items from the list using their index and storing it in a variable
 two = colors[1:3]
+
+# deleting the items from the list using del function
 del colors[1:3]
-print(two)
-print (len(colors))
-print(colors)
+
+# printing the stored variable
+print('Extracted', two, 'colors')
+
+# printing the length of updated list
+print ('The list contains',len(colors), 'items')
+
+# printing the updated list
+print('updated list', colors)
--- 03_Challenge_Lists/Advanced_Challenge/Advanced_Challenge_1.py
+++ 03_Challenge_Lists/Advanced_Challenge/Advanced_Challenge_1.py
@@ -6,8 +6,18 @@
 # Updated:     25-Feb-2025
 #---------------------------------------------------
 
+# Creating a fruit list
 fruits = ['banana', 'apple', 'grape', 'kiwi', 'orange']
+print ( fruits )
+
+# using the .sort to sort the items in ascending order
 fruits.sort()
+
+# printing the list in ascending order
 print(fruits, 'in ascending order')
+
+# using .reverse function to reverse the ascending order i.e now sorting in descending order
 fruits.reverse()
+
+# printing the list in descending order
 print(fruits, 'in descending order')
