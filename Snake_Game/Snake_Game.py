c4c42ab8c5ad3ecc960a7b484e1411f80dfa5b39
SV9050 <1090067@pdsb.net>
2025-04-24 12:48:50 -0400

    initial commit

--- Snake_Game/Snake_Game.py
+++ Snake_Game/Snake_Game.py
@@ -12,7 +12,7 @@ BLACK, WHITE, GREEN = (0, 0, 0), (255, 255, 255), (0, 255, 0)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Simple Snake Game")
 clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial", 15)
+font = pygame.font.SysFont("Arial", 24)
 
 # Draw messages
 def show_message(text, color, y_offset=0):
@@ -83,5 +83,3 @@ def game_loop():
 # Start the game
 show_intro()
 game_loop()
-
-
