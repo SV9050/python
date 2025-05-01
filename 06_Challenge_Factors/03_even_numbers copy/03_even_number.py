8031ff99b792f1b646c57ba159ef8faa1e2e1077
SV9050 <1090067@pdsb.net>
2025-05-01 13:43:36 -0400

    initial commit

---
+++ 06_Challenge_Factors/03_even_numbers copy/03_even_number.py
@@ -0,0 +1,16 @@
+#---------------------------------------------------
+# Name:        True or Else
+# Purpose:     to check if a variable is even number
+# Author:      Sachin
+# Created:     30-Apr-2025
+# Updated:     30-Apr-2025
+# ---------------------------------------------------
+
+def is_even(number):
+    return number % 2 == 0
+
+#  user input
+user_input = int(input("Enter a number: "))
+print(is_even(user_input))
+
+
